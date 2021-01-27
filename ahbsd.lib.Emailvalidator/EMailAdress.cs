834a9a0a2ee5001615692f98ec13a2f4d32c6f7b
Heinrich Alexandra Hermann <a.hermann@hermann-bsd.de>
2021-01-27 19:57:06 +0100

    Some changes
    Still developing…

--- ahbsd.lib.Emailvalidator/EMailAdress.cs
+++ ahbsd.lib.Emailvalidator/EMailAdress.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 using Newtonsoft.Json;
+using System.Net.Mail;
+using System.Text;
+using System.Linq;
+using System.Net;
+using System.Globalization;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http.Features;
 
 namespace ahbsd.lib.Emailvalidator
 {
     /// <summary>
     /// Class for an Email-Adress.
     /// </summary>
-    public partial class EMailAdress : IEMailAdress
+    public partial class EMailAdress : IEMailAdress, IGlobalizableFormat
     {
+        private MailAddress mailAddress;
+
         /// <summary>
         /// Constructor for a simple email adress.
         /// </summary>
@@ -20,6 +29,8 @@ namespace ahbsd.lib.Emailvalidator
             SLD = sld;
             Front = front;
             RestDomains = null;
+            MX = null;
+            mailAddress = new MailAddress($"{front}@{sld}.{tld}");
         }
 
         /// <summary>
@@ -31,10 +42,24 @@ namespace ahbsd.lib.Emailvalidator
         /// <param name="restDomains">Seperate domains.</param>
         protected EMailAdress(string front, string sld, string tld, string[] restDomains)
         {
+            string address;
+            StringBuilder restD = new StringBuilder(restDomains.Length * 2);
+
             TLD = tld;
             SLD = sld;
             Front = front;
             RestDomains = restDomains;
+            MX = null;
+
+            for (int i = 0; i < restDomains.Length; i++)
+            {
+                restD.Append(restDomains[i]);
+
+                restD.Append('.');
+            }
+
+            address = $"{front}@{restD}{sld}.{tld}";
+            mailAddress = new MailAddress(address);
         }
 
         #region implementation of IEmailAdress
@@ -63,6 +88,13 @@ namespace ahbsd.lib.Emailvalidator
         /// </summary>
         /// <value><c>TRUE</c>, if there are rest domains, otherwise <c>FALSE</c>.</value>
         public bool HasRestDomains => RestDomains != null;
+
+        /// <summary>
+        /// Gets the Mail-eXchanger, if available.
+        /// </summary>
+        /// <value>The Mail-eXchanger, if available, otherwise <c>NULL</c>.</value>
+        public string MX { get; private set; }
+
         /// <summary>
         /// Gets the email adress as JSon.
         /// </summary>
@@ -80,9 +112,100 @@ namespace ahbsd.lib.Emailvalidator
             }
 
             return result;
-         
+
+        }
+
+        /// <summary>
+        /// Gets or sets an email adress.
+        /// </summary>
+        /// <value>An email adress.</value>
+        public MailAddress MailAddress
+        {
+            get => mailAddress;
+            set
+            {
+                mailAddress = value;
+
+            }
         }
         #endregion
+
+        #region implementation of IGlobalizableFormat
+        /// <summary>
+        /// Gets the format string by the key.
+        /// Uses the current culture.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <returns>The globalized format string.</returns>
+        public string LocalizedFmt(string key)
+        {
+            string result = string.Empty;
+
+            return result;
+        }
+        /// <summary>
+        /// Gets the format string by the key for a defined culture.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <param name="culture">The defined culture.</param>
+        /// <returns>The globalized format string.</returns>
+        public string LocalizedFmt(string key, CultureInfo culture)
+        {
+            string result = string.Empty;
+
+            return result;
+        }
+        /// <summary>
+        /// Gets a list of all attributes used in the implementation.
+        /// </summary>
+        /// <returns>A list of all attributes.</returns>
+        public List<IErrorFormatAttribute> GetAttributes() => ErrorFormatAttributes;
+        /// <summary>
+        /// Gets the default JSON for the current object. To create initial
+        /// error message format globalization.
+        /// </summary>
+        /// <returns>The default JSON for the current object.</returns>
+        public string GetDefaultJson()
+        {
+            string result = "{}";
+
+
+            return result;
+        }
+        #endregion
+
+        /// <summary>
+        /// Gets a list of all attributes used in the implementation.
+        /// </summary>
+        /// <value>A list of all attributes.</value>
+        public static List<IErrorFormatAttribute> ErrorFormatAttributes
+        { get; private set; }
+
+        public static string ErrorFormat(CultureInfo ci, string key)
+        {
+            string result = string.Empty;
+
+            foreach (var item in ErrorFormatAttributes)
+            {
+                if (item.Key.Equals(key))
+                {
+
+                }
+            }
+
+
+            return result;
+        }
+
+        public static string ErrorFormat(string key)
+        {
+            string result = string.Empty;
+
+
+            return result;
+        }
+
+
 
         /// <summary>
         /// Gets an <see cref="IEMailAdress"/> from a given string.
@@ -90,12 +213,17 @@ namespace ahbsd.lib.Emailvalidator
         /// <param name="emailAdress">The emailadress as string.</param>
         /// <returns>The Email adress as object.</returns>
         /// <exception cref="WrongEmailException">If something is wrong.</exception>
+        [Globalizable(true)]
+        [ErrorFormat("GetEmailAdress")]
         public static IEMailAdress GetEmailAdress(string emailAdress)
         {
             IEMailAdress result = null;
             WrongEmailReason reason = WrongEmailReason.OK;
             string front, end, tld, sld;
             string[] domains, restdomains, parts;
+            string errorFmt;
+            Exception exception = null;
+            
 
 
             if (!emailAdress.Contains('@', StringComparison.Ordinal))
@@ -124,7 +252,10 @@ namespace ahbsd.lib.Emailvalidator
                 }
                 else
                 {
-                    throw new Exception(string.Format("Email adress '{0}' has a wrong syntax.\nSyntax: <front>@[<optional>.]<second level domain>.<toplevel domain>", emailAdress.Trim()));
+                    throw new Exception(string.Format("Email adress '{0}'" +
+                        " has a wrong syntax.\nSyntax: <front>@[<optional>.]" +
+                        "<second level domain>.<toplevel domain>",
+                        emailAdress.Trim()));
                 }
 
                 tld = domains[domains.Length - 1];
@@ -141,26 +272,63 @@ namespace ahbsd.lib.Emailvalidator
                         reason = WrongEmailReason.TLDWrong;
                     }
                 }
-
-                if (!ValidateSLD(sld))
+                else
                 {
-                    if (reason != WrongEmailReason.OK)
+                    string[] sldDotTld = new string[2];
+
+                    sldDotTld[0] = sld;
+                    sldDotTld[1] = tld;
+
+                    if (CheckDomain(sldDotTld, out exception))
                     {
-                        reason &= WrongEmailReason.SLDWrong;
+                        string[] all;
+
+                        switch (restdomains)
+                        {
+                            case null:
+                                result = new EMailAdress(front, sld, tld);
+                                break;
+                            default:
+                                all = RebuildDomain(tld, sld, restdomains);
+
+                                if (CheckDomain(all, out exception))
+                                {
+                                    result = new EMailAdress(
+                                        front,
+                                        sld,
+                                        tld,
+                                        restdomains);
+                                }
+                                else if(exception != null)
+                                {
+                                    reason = exception.GetType()
+                                        .Equals(typeof(OutOfMemoryException)) ?
+                                        WrongEmailReason.OutOfTime :
+                                        WrongEmailReason.SLDWrong;
+                                }
+                                else
+                                {
+                                    result = new EMailAdress(
+                                        front,
+                                        sld,
+                                        tld,
+                                        restdomains);
+                                }
+                                break;
+                        }
                     }
                     else
                     {
-                        reason = WrongEmailReason.SLDWrong;
+                        if (!exception.GetType()
+                            .Equals(typeof(TimeoutException)))
+                        {
+                            reason = AddSLDWrong(reason);
+                        }
+                        else
+                        {
+                            reason = WrongEmailReason.OutOfTime;
+                        }
                     }
-                }
-
-                if (restdomains == null)
-                {
-                    result = new EMailAdress(front, sld, tld);
-                }
-                else
-                {
-                    result = new EMailAdress(front, sld, tld, restdomains);
                 }
             }
 
@@ -169,21 +337,106 @@ namespace ahbsd.lib.Emailvalidator
                 throw new WrongEmailException(result, reason);
             }
 
+            result.MailAddress = new MailAddress(result.ToString());
 
             return result;
         }
 
+        /// <summary>
+        /// Gets an <see cref="IEMailAdress"/> from a given
+        /// <see cref="System.Net.Mail.MailAddress"/>.
+        /// </summary>
+        /// <param name="emailAdress">The email adress.</param>
+        /// <returns>The Email adress as object.</returns>
+        /// <exception cref="WrongEmailException">If something is wrong.</exception>
+        public static IEMailAdress GetEmailAdress(MailAddress emailAdress)
+            => GetEmailAdress(emailAdress.Address);
+
+        /// <summary>
+        /// Adds <see cref="WrongEmailReason.SLDWrong"/> to the given reason.
+        /// </summary>
+        /// <param name="reason">The given reason.</param>
+        /// <returns>The given reason with an added <see cref="WrongEmailReason.SLDWrong"/>.</returns>
+        /// <remarks>If the given reason is <see cref="WrongEmailReason.OK"/>
+        /// it will be comlete overwritten by
+        /// <see cref="WrongEmailReason.SLDWrong"/></remarks>
+        protected static WrongEmailReason AddSLDWrong(WrongEmailReason reason)
+        {
+            if (reason != WrongEmailReason.OK)
+            {
+                reason |= WrongEmailReason.SLDWrong;
+            }
+            else
+            {
+                reason = WrongEmailReason.SLDWrong;
+            }
+
+            return reason;
+        }
+
+        /// <summary>
+        /// Validates the Topleveldomain.
+        /// </summary>
+        /// <param name="tld">The Topleveldomain.</param>
+        /// <returns><c>TRUE</c> if the Topleveldomain is valid, otherwise <c>FALSE</c>.</returns>
         public static bool ValidateTLD(string tld)
         {
             return TLDs.Contains(tld.ToUpper());
         }
 
-        public static bool ValidateSLD(string sld)
+        protected static string[] RebuildDomain(string tld, string sld, string[] restdomains)
         {
-            bool result = true;
+            string[] all = new string[restdomains.Length + 2];
+            for (int i = 0; i < restdomains.Length; i++)
+            {
+                all[i] = restdomains[i];
+            }
 
+            all[restdomains.Length] = sld;
+            all[restdomains.Length + 1] = tld;
+            return all;
+        }
 
-            return result;
+        protected static string RebuildDomain(string[] nameArray)
+        {
+#if DEBUG
+            StringBuilder result = new StringBuilder(nameArray.Length);
+            int tmp = nameArray.Length - 1;
+
+            for (int i = 0; i < tmp; i++)
+            {
+                _ = result.AppendFormat("{0}.");
+            }
+
+            _ = result.Append(nameArray[tmp]);
+
+            return result.ToString();
+#else
+            return string.Join('.', nameArray);
+#endif
+        }
+
+        /// <summary>
+        /// Returns the email adress.
+        /// </summary>
+        /// <returns>The email adress.</returns>
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder(Front);
+
+            result.Append('@');
+
+            if (RestDomains != null)
+            {
+                for (int i = 0; i < RestDomains.Length; i++)
+                {
+                    result.AppendFormat("{0}.", RestDomains[i]);
+                }
+            }
+
+            _ = result.AppendFormat("{0}.{1}", SLD, TLD);
+
+            return result.ToString();
         }
     }
 }
--- ahbsd.lib.Emailvalidator/Emailvalidator.LDCheck.cs
+++ ahbsd.lib.Emailvalidator/Emailvalidator.LDCheck.cs
@@ -1,6 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
 using RestSharp;
+using Whois;
+using Whois.Servers;
+using Walter.Net;
+using Walter.Net.Networking;
+using Walter.Net.Networking.DNS;
+using Walter.Net.Networking.IANA;
+using System.Globalization;
+using System.IO;
+using Newtonsoft.Json;
+using RestSharp.Serialization.Json;
 
 namespace ahbsd.lib.Emailvalidator
 {
@@ -9,23 +20,66 @@ namespace ahbsd.lib.Emailvalidator
     /// </summary>
     public partial class EMailAdress
     {
+
+        readonly static string PathToCultureList; 
         /// <summary>
-        /// Gets or sets a List of Top Level Domains.
+        /// Gets a List of Top Level Domains.
         /// </summary>
         protected static List<string> TLDs { get; private set; }
+        /// <summary>
+        /// Gets a Dictionary of available Cultures for globalized Messanges.
+        /// </summary>
+        /// <value>A Dictionary of available Cultures.</value>
+        protected static Dictionary<CultureTypes, IDictionary<string,string>> Cultures { get; private set; }
 
+        /// <summary>
+        /// Static constructor.
+        /// </summary>
+        [Globalizable(true)]
         static EMailAdress()
         {
+            string fullPath;
+            PathToCultureList = Path.GetFullPath("../coltureLists/");
+
+            fullPath = TestPath();
+
+            Cultures = new Dictionary<CultureTypes, IDictionary<string,string>>();
             TLDs = new List<string>();
+
+            GetCultures(fullPath);
             GetTLDs();
         }
 
+        private static string TestPath()
+        {
+            string result = PathToCultureList;
+            DirectoryInfo tmp;
+
+            if (!Directory.Exists(PathToCultureList))
+            {
+                try
+                {
+                    tmp = Directory.CreateDirectory(PathToCultureList);
+
+                    result = tmp.FullName;
+                }
+                catch (Exception)
+                { }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Gets all known Topleveldomains from IANA.
+        /// </summary>
         private static void GetTLDs()
         {
             IRestClient client = new RestClient("https://data.iana.org/TLD/");
             IRestRequest request = new RestRequest("tlds-alpha-by-domain.txt");
             IRestResponse response = client.Get(request);
             string[] tmp;
+
 
             if (response.IsSuccessful)
             {
@@ -38,6 +92,78 @@ namespace ahbsd.lib.Emailvalidator
                     TLDs.Add(tmp[i]);
                 }
             }
+        }
+
+        private static void GetCultures(string path)
+        {
+            DirectoryInfo di = new DirectoryInfo(path);
+            IEnumerable<FileInfo> files = di.EnumerateFiles();
+            StreamReader reader;
+            string tmpString;
+            object tmpObject;
+            CultureInfo tmpCulture;
+
+            foreach (FileInfo file in files)
+            {
+                if (file.Extension.ToLower().Equals(".json"))
+                {
+                    reader = file.OpenText();
+
+                    tmpString = reader.ReadToEnd();
+                    reader.Close();
+                    tmpObject = JsonConvert.DeserializeObject(tmpString);
+
+                    if (tmpObject != null)
+                    {
+
+                    }
+
+                }
+            }
+        }
+
+        public static bool CheckDomain(string fullDomain, out Exception exception)
+        {
+            bool result = false;
+            string dnsTmp;
+#if DEBUG
+            // not really needed but nice for debugging and testing.
+            IList<string> servers, stati;
+
+            IWhoisServerLookup wsl;
+            WhoisResponse response;
+#endif
+            WhoisLookup lookup = new WhoisLookup();
+            lookup.Options.FollowReferrer = true;
+            lookup.Options.TimeoutSeconds = 20;
+
+            exception = null;
+
+            try
+            {
+                response = lookup.Lookup(fullDomain);
+                result = response.Status == Whois.WhoisStatus.Found;
+#if DEBUG
+                wsl = lookup.ServerLookup;
+
+                dnsTmp = response.DnsSecStatus;
+                servers = response.NameServers;
+                stati = response.DomainStatus;
+#endif
+            }
+            catch (Exception ex)
+            {
+                exception = ex;
+            }
+
+            return result;
+        }
+
+        public static bool CheckDomain(string[] domainParts, out Exception exception)
+        {
+            string tmp = string.Join('.', domainParts);
+
+            return CheckDomain(tmp, out exception);
         }
     }
 }
