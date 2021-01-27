c1782881dda1fd5c482c689339e204c380dc6714
Heinrich Alexandra Hermann <a.hermann@hermann-bsd.de>
2021-01-27 19:55:29 +0100

    Added some code
    Code for testing the library

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ahbsd.lib.Emailvalidator;
 
 namespace Test
 {
@@ -7,6 +8,15 @@ namespace Test
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            IEMailAdress[] adresses = new IEMailAdress[3];
+
+            adresses[0] = EMailAdress.GetEmailAdress("alex.hermann@icloud.com");
+            adresses[1] = EMailAdress.GetEmailAdress("alex.hermann@test1.hermann-bsd.de");
+            adresses[2] = EMailAdress.GetEmailAdress("alex.hermann@test2.test1.hermann-bsd.de");
+
+            string adr = adresses[0].GetJSon();
+            adr = adresses[1].GetJSon();
+            adr = adresses[2].GetJSon();
         }
     }
 }
